eeb0a6b2db59e3665a6a28704c5f1d319cdd93a8
reshinto <terencekong2002@gmail.com>
2019-07-19 21:29:12 +0800

    Add basic navbar styles

--- frontend/trading_platform/src/components/Navbar.js
+++ frontend/trading_platform/src/components/Navbar.js
@@ -1,32 +1,395 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
-import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../redux/actions/authAction";
+// import PropTypes from "prop-types";
+// import { withStyles } from "@material-ui/core/styles";
+// import AppBar from "@material-ui/core/AppBar";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import Typography from "@material-ui/core/Typography";
+// import IconButton from "@material-ui/core/IconButton";
+// import MenuIcon from "@material-ui/icons/Menu";
+// import AccountCircle from "@material-ui/icons/AccountCircle";
+// import MenuItem from "@material-ui/core/MenuItem";
+// import Menu from "@material-ui/core/Menu";
+// import Button from "@material-ui/core/Button";
+
+// const styles = {
+//   root: {
+//     flexGrow: 1
+//   },
+//   grow: {
+//     flexGrow: 1
+//   },
+//   menuButton: {
+//     marginLeft: -12,
+//     marginRight: 20
+//   }
+// };
+
+// class Navbar extends React.Component {
+//   state = {
+//     anchorEl: null
+//   };
+
+//   handleMenu = event => {
+//     this.setState({ anchorEl: event.currentTarget });
+//   };
+
+//   handleClose = () => {
+//     this.setState({ anchorEl: null });
+//   };
+
+//   render() {
+//     const { isAuthenticated, classes } = this.props;
+//     const { anchorEl } = this.state;
+//     const open = Boolean(anchorEl);
+
+//     return (
+//       <div className={classes.root}>
+//         <AppBar style={{ backgroundColor: "#323232" }} position="static">
+//           <Toolbar>
+//             <IconButton
+//               className={classes.menuButton}
+//               color="inherit"
+//               aria-label="Menu"
+//             >
+//               <MenuIcon />
+//             </IconButton>
+//             <Typography variant="h6" color="inherit" className={classes.grow}>
+//               <Button color="inherit" component={Link} to="/">
+//                 Dashboard
+//               </Button>
+//             </Typography>
+//             {isAuthenticated ? (
+//               <div>
+//                 <IconButton
+//                   aria-owns={open ? "menu-appbar" : undefined}
+//                   aria-haspopup="true"
+//                   onClick={this.handleMenu}
+//                   color="inherit"
+//                 >
+//                   <AccountCircle />
+//                 </IconButton>
+//                 <Menu
+//                   id="menu-appbar"
+//                   anchorEl={anchorEl}
+//                   anchorOrigin={{
+//                     vertical: "top",
+//                     horizontal: "right"
+//                   }}
+//                   transformOrigin={{
+//                     vertical: "top",
+//                     horizontal: "right"
+//                   }}
+//                   open={open}
+//                   onClose={this.handleClose}
+//                 >
+//                   <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+//                   <MenuItem onClick={this.handleClose}>My account</MenuItem>
+//                   <MenuItem onClick={this.props.logout}>Logout</MenuItem>
+//                 </Menu>
+//               </div>
+//             ) : (
+//               <div>
+//                 <Button color="inherit" component={Link} to="/login">
+//                   Login
+//                 </Button>
+//                 <Button color="inherit" component={Link} to="/signup">
+//                   Signup
+//                 </Button>
+//               </div>
+//             )}
+//           </Toolbar>
+//         </AppBar>
+//         {this.props.children}
+//       </div>
+//     );
+//   }
+// }
+
+// Navbar.propTypes = {
+//   classes: PropTypes.object.isRequired
+// };
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     logout: () => dispatch(actions.logout())
+//   };
+// };
+
+// export default withRouter(
+//   connect(
+//     null,
+//     mapDispatchToProps
+//   )(withStyles(styles)(Navbar))
+// );
+
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import { withStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import Button from "@material-ui/core/Button";
+import LogoutIcon from "@material-ui/icons/ExitToApp";
+
+const styles = theme => ({
+  root: {
+    width: "100%"
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing.unit * 3,
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit",
+    width: "100%"
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200
+    }
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex"
+    }
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
+  }
+});
 
 class Navbar extends React.Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <Link to="/">Home</Link>
-        </div>
-        {this.props.isAuthenticated ? (
-          <button onClick={this.props.logout}>Logout</button>
-        ) : (
-          <div>
-            <div>
-              <Link to="/login">Login</Link>
-            </div>
+  state = {
+    anchorEl: null,
+    mobileMoreAnchorEl: null
+  };
 
-            <NavLink to="/signup/">Signup</NavLink>
-          </div>
-        )}
+  handleProfileMenuOpen = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleMenuClose = () => {
+    this.setState({ anchorEl: null });
+    this.handleMobileMenuClose();
+  };
+
+  handleMobileMenuOpen = event => {
+    this.setState({ mobileMoreAnchorEl: event.currentTarget });
+  };
+
+  handleMobileMenuClose = () => {
+    this.setState({ mobileMoreAnchorEl: null });
+  };
+
+  render() {
+    const { anchorEl, mobileMoreAnchorEl } = this.state;
+    const { isAuthenticated, classes } = this.props;
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const renderMenu = (
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        open={isMenuOpen}
+        onClose={this.handleMenuClose}
+      >
+        <div>
+          <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
+          <MenuItem onClick={this.handleMenuClose}>My account</MenuItem>
+          <MenuItem onClick={this.props.logout}>Logout</MenuItem>
+        </div>
+      </Menu>
+    );
+
+    const renderMobileMenu = (
+      <Menu
+        anchorEl={mobileMoreAnchorEl}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        open={isMobileMenuOpen}
+        onClose={this.handleMenuClose}
+      >
+        <MenuItem onClick={this.handleMobileMenuClose}>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <MailIcon />
+            </Badge>
+          </IconButton>
+          <p>Messages</p>
+        </MenuItem>
+        <MenuItem onClick={this.handleMobileMenuClose}>
+          <IconButton color="inherit">
+            <Badge badgeContent={11} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <p>Notifications</p>
+        </MenuItem>
+        <MenuItem onClick={this.handleMobileMenuClose}>
+          <IconButton color="inherit">
+            <AccountCircle />
+          </IconButton>
+          <p>Profile</p>
+        </MenuItem>
+        <div onClick={this.props.logout}>
+          <MenuItem onClick={this.handleMobileMenuClose}>
+            <IconButton color="inherit">
+              <LogoutIcon />
+            </IconButton>
+            <p>Logout</p>
+          </MenuItem>
+        </div>
+      </Menu>
+    );
+
+    return (
+      <div className={classes.root}>
+        <AppBar style={{ backgroundColor: "#323232" }} position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              className={classes.title}
+              variant="h6"
+              color="inherit"
+              noWrap
+            >
+              <Button color="inherit" component={Link} to="/">
+                Dashboard
+              </Button>
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+              />
+            </div>
+            <div className={classes.grow} />
+            {isAuthenticated ? (
+              <div>
+                <div className={classes.sectionDesktop}>
+                  <IconButton color="inherit">
+                    <Badge badgeContent={4} color="secondary">
+                      <MailIcon />
+                    </Badge>
+                  </IconButton>
+                  <IconButton color="inherit">
+                    <Badge badgeContent={17} color="secondary">
+                      <NotificationsIcon />
+                    </Badge>
+                  </IconButton>
+                  <IconButton
+                    aria-owns={isMenuOpen ? "material-appbar" : undefined}
+                    aria-haspopup="true"
+                    onClick={this.handleProfileMenuOpen}
+                    color="inherit"
+                  >
+                    <AccountCircle />
+                  </IconButton>
+                </div>
+                <div className={classes.sectionMobile}>
+                  <IconButton
+                    aria-haspopup="true"
+                    onClick={this.handleMobileMenuOpen}
+                    color="inherit"
+                  >
+                    <MoreIcon />
+                  </IconButton>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <Button color="inherit" component={Link} to="/signup">
+                  Signup
+                </Button>
+                <Button color="inherit" component={Link} to="/login">
+                  Login
+                </Button>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+        {renderMenu}
+        {renderMobileMenu}
         {this.props.children}
       </div>
     );
   }
 }
+
+Navbar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -38,5 +401,5 @@ export default withRouter(
   connect(
     null,
     mapDispatchToProps
-  )(Navbar)
+  )(withStyles(styles)(Navbar))
 );
